22aa484aea6170699f9134e0500bb16daf52801d
Marco Pacini <marco.pacini@adevinta.com>
2020-06-21 00:06:08 +0200

    Run deno fmt

--- registry_test.ts
+++ registry_test.ts
@@ -1,9 +1,9 @@
 import {
   assertEquals,
-  test
+  test,
 } from "./test_deps.ts";
 
-import { Registry } from "./registry.ts"
+import { Registry } from "./registry.ts";
 import { Collector } from "./collector.ts";
 
 test({
@@ -11,7 +11,7 @@ test({
   fn() {
     const registry = new Registry();
     const collector1 = new Collector("collector1", "help1", "type");
-  
+
     registry.register(collector1);
     const metricsText = "# HELP collector1 help1\n# TYPE collector1 type\n";
     assertEquals(registry.metrics(), metricsText);
@@ -22,5 +22,5 @@ test({
     registry.register(collector1);
     registry.clear();
     assertEquals(registry.metrics(), "");
-  }
-})
+  },
+});
