e4843900a71b90e3d3b00b764b5ef9f1c914d2b2
Marco Pacini <marco.pacini@adevinta.com>
2021-02-01 11:31:29 +0100

    Closes #3

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 export { Counter } from "./counter.ts";
 export { Gauge } from "./gauge.ts";
 export { Histogram } from "./histogram.ts";
-export { Metric, Inc, Dec, Set, Observe } from "./metric.ts";
+export { Summary } from "./summary.ts"
+export type { Metric, Inc, Dec, Set, Observe } from "./metric.ts";
 export { Registry } from "./registry.ts";
